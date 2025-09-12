4c1b64a93d5d87d05142d3408f48b97e643a3cb2
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-12 15:49:35 +0200

    create a class Person and func create_person_list №2

--- app/main.py
+++ app/main.py
@@ -11,7 +11,9 @@ def create_person_list(people: list) -> list:
             Person(person["name"], person["age"])
     for pers in people:
         if "wife" in pers and pers["wife"] is not None:
-            Person.people[pers["name"]].wife = Person.people[pers["wife"]]
+            Person.people[pers["name"]].wife \
+                = Person.people[pers["wife"]]
         elif "husband" in pers and pers["husband"] is not None:
-            Person.people[pers["name"]].husband = Person.people[pers["husband"]]
+            Person.people[pers["name"]].husband \
+                = Person.people[pers["husband"]]
     return list(Person.people.values())
